ab8c2e39faca9acb8ad96be047e5a1b686972824
wonder13662 <wonder13662@gmail.com>
2021-03-02 15:54:00 +0900

    Init commit

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
+## Apollo Docs
+[Apollo Docs Client(React)](https://www.apollographql.com/docs/react/get-started/)
+
 ## Available Scripts
 
 In the project directory, you can run:
--- src/index.js
+++ src/index.js
@@ -1,12 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import {
+  ApolloClient,
+  InMemoryCache,
+  useQuery,
+  gql,
+  ApolloProvider
+} from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+  cache: new InMemoryCache()
+});
+
+const EXCHANGE_RATES = gql`
+  query GetExchangeRates {
+    rates(currency: "USD") {
+      currency
+      rate
+    }
+  }
+`;
+
+function ExchangeRates() {
+  const { loading, error, data } = useQuery(EXCHANGE_RATES);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return data.rates.map(({ currency, rate }) => (
+    <div key={currency}>
+      <p>
+        {currency}: {rate}
+      </p>
+    </div>
+  ));
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <div>
+        <h2>My first Apollo app 🚀</h2>
+        <ExchangeRates />
+      </div>
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
